d248aeffd51b699decdfd11ffabd146ac2dec057
pmsoo <pmsoo4@naver.com>
2025-01-31 09:36:25 +0900

    Initial

--- D0128/src/main/java/util/ConnectionPool.java
+++ D0128/src/main/java/util/ConnectionPool.java
@@ -6,10 +6,10 @@ import javax.sql.*;
 
 public class ConnectionPool {
     private static DataSource _ds = null;
-	
+
     public static Connection get() throws NamingException, SQLException {
         if (_ds == null) {
-            _ds = (DataSource) (new InitialContext()).lookup("java:comp/env/jdbc/mysns");
+            _ds = (DataSource) (new InitialContext()).lookup("java:comp/env/jdbc/oracleDB"); 
         }
         return _ds.getConnection();
     }
