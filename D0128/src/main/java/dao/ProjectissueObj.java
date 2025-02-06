d12e0830befaaf173f8dc4936e12578393a44c21
w3mil <w3mil@TABLET-KGA3DP50>
2025-02-06 17:02:10 +0900

    project issue jsp

--- D0128/src/main/java/dao/ProjectissueObj.java
+++ D0128/src/main/java/dao/ProjectissueObj.java
@@ -3,15 +3,17 @@ import java.sql.Timestamp;
 
 public class ProjectissueObj {
     private int projectIssueId;  // 이슈의 고유 ID
-    private int projectUserId;   // 이슈를 등록한 사용자 ID
+    private String projectUserId;   // 이슈를 등록한 사용자 ID
+    private int projectId;
     private String title;         // 이슈의 제목
     private String description;   // 이슈에 대한 설명
     private Timestamp createdAt;  // 이슈 생성일 및 시간
 
     // 생성자
-    public ProjectissueObj(int projectIssueId, int projectUserId, String title, String description, Timestamp createdAt) {
+    public ProjectissueObj(int projectIssueId, String projectUserId, int projectId, String title, String description, Timestamp createdAt) {
         this.projectIssueId = projectIssueId;
         this.projectUserId = projectUserId;
+        this.projectId = projectId;
         this.title = title;
         this.description = description;
         this.createdAt = createdAt;
@@ -22,8 +24,12 @@ public class ProjectissueObj {
         return projectIssueId;
     }
 
-    public int getProjectUserId() {
+    public String getProjectUserId() {
         return projectUserId;
+    }
+    
+    public int getProjectId() {
+        return projectId;
     }
 
     public String getTitle() {
